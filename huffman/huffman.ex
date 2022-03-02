3d477d05824312215469d4b704c2be46371a2c33
Digitus <62904448+Digitusmedia@users.noreply.github.com>
2022-03-02 15:11:40 +0100

    some fixes

---
+++ huffman/huffman.ex
@@ -0,0 +1,230 @@
+defmodule Huffman do
+
+  #c("huffman.ex") && Huffman.tree(Huffman.sample)
+  #c("huffman.ex") && Huffman.tree(Huffman.text)
+  #c("huffman.ex") && Huffman.test2()
+
+
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000)
+  end
+
+  #def read(file) do
+  #  {:ok, file} = File.open(file, [:read, :utf8])
+  #  binary = IO.read(file, :all)
+  #  File.close(file)
+
+  #  case :unicode.characters_to_list(binary, :utf8) do
+  #    {:incomplete, list, _} -> list
+  #    list -> list
+  #  end
+  #end
+
+  def read(file, n) do
+    {:ok, fd} = File.open(file, [:read, :utf8])
+    binary = IO.read(fd, n)
+    File.close(fd)
+
+    length = byte_size(binary)
+    case :unicode.characters_to_list(binary, :utf8) do
+      {:incomplete, chars, rest} ->
+        {chars, length - byte_size(rest)}
+      chars ->
+        {chars, length}
+    end
+  end
+
+
+  def sample do
+    'the quick brown fox jumps over the lazy dog
+this is a sample text that we will use when we build
+up a table we will only handle lower case letters and
+no punctuation symbols the frequency will of course not
+represent english but it is probably not that far off'
+  end
+
+  def text() do
+    'this is something that we should encode'
+  end
+
+  def test1 do
+    sample = sample()
+    tree = tree(sample)
+    codes(tree, [])
+  end
+  def test2 do
+    sample = sample()
+    tree = tree(sample)
+    encode_table(tree)
+  end
+  def test3 do
+    sample = sample()
+    tree = tree(sample)
+    encode = encode_table(tree)
+    encode(text,encode)
+  end
+
+  def test do
+    sample = sample()
+    tree = tree(sample)
+    encode = encode_table(tree)
+    decode = decode_table(tree)
+    text = text()
+    seq = encode(sample, encode)
+    seq = encode(text, encode)
+    decode(seq, decode)
+  end
+
+  #c("huffman.ex") && Huffman.measure_test("kallocain.txt", 300_000)
+  def measure_test(file, n) do
+    {text, b} = read(file, n)
+    sample = sample()
+    c = length(text)
+    {tree, t2} = time(fn -> tree(text) end)
+    {encode, t3} = time(fn -> encode_table(tree) end)
+    s = length(encode)
+    {decode, _} = time(fn -> decode_table(tree) end)
+    {encoded, t5} = time(fn -> encode(text, encode) end)
+    e = div(length(encoded), 8)
+    r = Float.round(e / b, 3)
+    {_, t6} = time(fn -> decode(encoded, decode) end)
+
+    IO.puts("text of #{c} characters")
+    IO.puts("tree built in #{t2} ms")
+    IO.puts("table of size #{s} in #{t3} ms")
+    IO.puts("encoded in #{t5} ms")
+    IO.puts("decoded in #{t6} ms")
+    IO.puts("source #{b} bytes, encoded #{e} bytes, compression #{r}")
+
+  end
+
+  def time(func) do
+    initial = Time.utc_now()
+    result = func.()
+    final = Time.utc_now()
+    {result, Time.diff(final, initial, :microsecond) / 1000}
+  end
+
+  def freq(sample) do
+    freq(sample, [])
+  end
+
+  def freq([], freq) do
+    freq
+  end
+
+  def freq([char | rest], freq) do
+    freq(rest, count_char(char, freq))
+  end
+
+  def count_char(char, []) do # issue
+    [{char, 1}]
+  end
+  def count_char(char, list) do
+    [h1 | t1] = list
+    {h2, t2} = h1
+    cond do
+      char === h2 -> [{h2, t2 + 1} | t1]
+      true -> [h1] ++ count_char(char, t1)
+    end
+  end
+
+  def tree(sample) do
+    freq = freq(sample)
+    huffman(freq)
+  end
+
+  def huffman(freq) do
+    sorted = Enum.sort(freq, fn({_, x}, {_, y}) -> x < y end)
+    huffman_tree(sorted)
+  end
+
+
+  def huffman_tree([{tree, _}]) do tree end
+  def huffman_tree([{char1, char1_freq}, {char2, char2_freq} | rest]) do
+    huffman_tree(insert({{char1, char2}, char1_freq + char2_freq}, rest))
+  end
+
+  def insert({char, freq}, []) do [{char, freq}] end
+  def insert({char1, char1_freq}, [{char2, char2_freq} | rest]) when char1_freq < char2_freq do
+    [{char1, char1_freq}, {char2, char2_freq} | rest]
+  end
+  def insert({char1, char1_freq}, [{char2, char2_freq} | rest]) do
+    [{char2, char2_freq} | insert({char1, char1_freq}, rest)]
+  end
+
+  def codes({elem_1, elem_2}, binary_codes) do
+    left = codes(elem_1, [0 | binary_codes])
+    right = codes(elem_2, [1 | binary_codes])
+    left ++ right
+  end
+  def codes( elem, code) do
+    [{elem, Enum.reverse(code)}]
+  end
+
+
+  def encode_table(tree) do
+    Enum.sort(codes(tree, []), fn({_,x},{_,y}) -> length(x) < length(y) end)
+  end
+
+  def decode_table(tree) do
+    codes(tree, [])
+  end
+
+  def encode([], _), do: []
+  def encode([char | rest], table) do
+    {_, code} = List.keyfind(table, char, 0)
+    code ++ encode(rest, table)
+  end
+
+
+  #def decode(seq, table) do
+  #  {char, rest} = decode_char(seq, 1, table)
+  #  [char | decode(rest, table)]
+  #end
+  #def decode_char(seq, n, table) do
+  #  {code, rest} = Enum.split(seq, n)
+  #  case List.keyfind(table, code, 1) do
+  #    {char, _} ->
+  #      {char, rest};
+  #    nil ->
+  #      decode_char(seq, n+1, table)
+  #  end
+  #end
+
+
+
+  def decode(sequence, table) do
+    decoder(sequence, table, [], [])
+  end
+
+  def decoder([], table, check, message) do
+    Enum.reverse(message)
+  end
+  def decoder(encoded, table, check, message) do
+    [head | tail] = encoded
+    new_check = check ++ [head]
+    decode_answer = decode_checker(new_check, table)
+    case decode_answer do
+      [nil] -> decoder(tail, table, new_check, message)
+      _ -> decoder(tail, table, [], [decode_answer] ++ message)
+    end
+  end
+
+
+  def decode_checker(code, []) do
+    [nil]
+  end
+  def decode_checker(code, table) do
+    [head | tail] = table
+    {element, element_code} = head
+    cond do
+      code === element_code -> element
+      true -> decode_checker(code, tail)
+    end
+  end
+
+end
--- train_shunting/train.ex
+++ train_shunting/train.ex
@@ -8,7 +8,6 @@ defmodule Shunt do
 
   def split(list, atom) do
     case Lists.member(list, atom) do
-      false -> :ops
       true -> {Lists.take(list, Lists.position(list, atom)-1), Lists.drop(list, Lists.position(list, atom))}
     end
   end
@@ -28,7 +27,7 @@ defmodule Shunt do
   end
 
 
-  def few([u|k], [u|t]) do few(k, t) end
+  def few([head|k], [head|t]) do few(k, t) end
   def few(xs, ys) do
     case ys do
       [] -> []
@@ -38,10 +37,53 @@ defmodule Shunt do
         step = Moves.single({:two, length(hs)}, step)
         step = Moves.single({:one, -(length(ts)+1)}, step)
         {[_|t2], [], []} = Moves.single({:two, -(length(hs))}, step)
-
         [{:one, length(ts)+1}, {:two, length(hs)}, {:one, -(length(ts)+1)}, {:two, -(length(hs))}| few(t2, t)]
     end
   end
+
+  def compress([])do [] end
+  def compress(moves)do
+
+    moves_compressed = rules(moves)
+
+    case moves_compressed == moves do
+      true -> moves
+      false -> compress(moves_compressed)
+    end
+  end
+
+  def rules([]) do [] end
+
+  def rules([{_, 0} | []]) do [] end
+
+  def rules([xs | []]) do [xs] end
+
+  def rules([{_, 0} | tail]) do rules(tail) end
+
+  def rules([{track1, order1},{_, 0} | tail])do
+    rules([{track1, order1} | tail])
+  end
+  def rules([{track1, order1},{track1, order2}| tail])do
+    rules([{track1, order1 + order2} | tail])
+  end
+  def rules([{track1, order1},{track2, order2}| tail])do
+    [{track1, order1} | rules([{track2, order2} | tail])]
+  end
+
+
+  #def rules([]) do [] end
+  #def rules([_, 0]) do [] end
+  #def rules([{track_num,0} | rest]) do
+  #  rules(rest)
+  #end
+  #def rules([{a,b}]) do [a,b] end
+  #def rules([{track_num, num}, {track_num_num, num_num} | rest] = ms) do
+  #  if track_num == track_num_num and num + num_num == 0 do
+  #    rules(Lists.append([{track_num, num + num_num}], rest)) else
+  #    Lists.append([{track_num,num}], [rules([{track_num_num, num_num}|rest])])
+  #  end
+  #end
+
 end
 
 #cd("train_shunting") && c("train.ex")
@@ -76,23 +118,31 @@ defmodule Lists do
       true -> 1 + position(t, y)
     end
   end
-
 end
 #c("train.ex") && Moves.move([{:one,1},{:two,1},{:one,-1}],{[:a,:b],[],[]})
 defmodule Moves do
-  def single({:one,n},{main,one,two}) do
-    case n do
-      0 -> {main,one,two}
-      -1 -> {Lists.append(main,Lists.take(one,1)), Lists.drop(one,1),two}
-      1 -> {Lists.take(main,length(main)-n), Lists.drop(Lists.append(main,one),length(main)-n),two}
+  def single({:one, n}, {main, one, two}) do
+    cond do
+      length(main) - n < 0 ->
+        {main, one, two}
+      n < 0 ->
+        {Lists.append(main, Lists.take(one, n*-1)), Lists.drop(one, n * -1), two}
+      n > 0 ->
+        {Lists.take(main, length(main)-n), Lists.append(Lists.drop(main, length(main)-n), one), two}
+      true -> {main, one, two}
     end
   end
 
-  def single({:two,n},{main,one,two})do
-    case n do
-      0 -> {main, one, two}
-      -1 -> {Lists.append(main,Lists.take(two,1)),one,Lists.drop(two,1)}
-      1 -> {Lists.take(main,length(main)-n),one,Lists.append(Lists.drop(main,length(main)-n), two)}
+
+  def single({:two, n}, {main, one, two}) do
+    cond do
+      length(main) - n < 0 ->
+        {main, one, two}
+      n < 0 ->
+        {Lists.append(main, Lists.take(two, n*-1)), one, Lists.drop(two, n*-1)}
+      n > 0 ->
+        {Lists.take(main, length(main)-n), one, Lists.append(Lists.drop(main, length(main)-n), two)}
+      true -> {main, one, two}
     end
   end
   def move([], state) do [state] end
